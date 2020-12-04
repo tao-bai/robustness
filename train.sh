a9a0e39c472085dad60cb092409cbf85b51f6755
taobai <taobai2078@gmail.com>
2020-12-04 18:29:37 +0800

    small modification

---
+++ train.sh
@@ -0,0 +1,2 @@
+# python -m robustness.main --dataset cifar --data ./data --adv-train 0 --arch resnet18 --out-dir ./logs/checkpoints/dir/
+python -m robustness.main --dataset cifar --data ./data --adv-train 0 --arch resnet50 --out-dir ./logs/checkpoints/dir/ --exp-name resnet50-cifar10
